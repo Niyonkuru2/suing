87472840e29eb210c2d8cff808bd109cbade0b0d
Niyonkuru2 <150118511+Niyonkuru2@users.noreply.github.com>
2026-09-09 02:54:06 +0200

    Change cron schedule to run analysis hourly

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.listen(PORT, () => {
 });
 
 // Run automatic analysis every 5 minutes
-cron.schedule("*/5 * * * *", async () => {
+//cron.schedule("*/5 * * * *", async () => {
+cron.schedule("0 * * * *", async () => {
   console.log("Running scheduled market analysis...");
 await autoAnalyzeMarket();
 });
